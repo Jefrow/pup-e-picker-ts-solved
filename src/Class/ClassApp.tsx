fee6d77144951184924fe9c34b7ef0a7d21ab501
Jethro Coma <jethrocoma@Jethros-MacBook-Pro.local>
2024-09-21 00:29:27 -0700

    Refactor funcntional App to work with Class app

--- src/Class/ClassApp.tsx
+++ src/Class/ClassApp.tsx
@@ -2,19 +2,88 @@ import { Component } from "react";
 import { ClassSection } from "./ClassSection";
 import { ClassDogs } from "./ClassDogs";
 import { ClassCreateDogForm } from "./ClassCreateDogForm";
+import { Dog } from "../types";
+import { Requests } from "../api";
 
-export class ClassApp extends Component {
+type State = {
+  allDogs: Dog[] | [];
+  activeTab: string;
+  isLoading: boolean;
+};
+export class ClassApp extends Component<State> {
+  state: State = {
+    allDogs: [],
+    activeTab: "all",
+    isLoading: false,
+  };
+
+  refetchData = () => {
+    this.setState({ isLoading: true });
+    return Requests.getAllDogs()
+      .then((dog) => {
+        this.setState({ allDogs: dog });
+      })
+      .finally(() => this.setState({ isLoading: false }));
+  };
+
+  componentDidMount(): void {
+    this.refetchData();
+  }
+
+  createDog = (dog: Omit<Dog, "id">) => {
+    this.setState({ isLoadig: true });
+    Requests.postDog(dog)
+      .then(() => this.refetchData())
+      .finally(() => this.setState({ isLoading: false }));
+  };
+
+  deleteDog = (dog: Dog) => {
+    this.setState({ isLoading: true });
+    Requests.deleteDog(dog)
+      .then(() => this.refetchData())
+      .finally(() => this.setState({ isLoading: false }));
+  };
+
+  updateDog = (dog: Pick<Dog, "id" | "isFavorite">) => {
+    this.setState({ isLoading: true });
+    Requests.updateDog(dog)
+      .then(() => this.refetchData())
+      .finally(() => this.setState({ isLoading: false }));
+  };
+
   render() {
+    const { activeTab, allDogs, isLoading } = this.state;
+    const favorited = allDogs.filter((dog) => dog.isFavorite).length;
+    const unfavorited = allDogs.length - favorited;
+
     return (
       <div className="App" style={{ backgroundColor: "goldenrod" }}>
         <header>
           <h1>pup-e-picker (Class Version)</h1>
         </header>
-        <ClassSection />
+        <ClassSection
+          setActiveTab={(tab) => this.setState({ activeTab: tab })}
+          favorited={favorited}
+          unfavorited={unfavorited}
+          activeTab={activeTab}
+        >
+          {activeTab === "create" ? (
+            <ClassCreateDogForm
+              createDog={this.createDog}
+              isLoading={isLoading}
+            />
+          ) : (
+            <ClassDogs 
+              allDogs={allDogs}
+              filter={activeTab}
+              isLoading={isLoading}
+              deleteDog={this.deleteDog}
+              updateDog={this.updateDog}
+            />
+          )}
+        </ClassSection>
 
         {/* should be inside of the ClassSection component using react children */}
-        <ClassDogs />
-        <ClassCreateDogForm />
       </div>
     );
   }
--- src/Class/ClassCreateDogForm.tsx
+++ src/Class/ClassCreateDogForm.tsx
@@ -1,30 +1,85 @@
 import { Component } from "react";
 import { dogPictures } from "../dog-pictures";
+import { Dog } from "../types";
 
-export class ClassCreateDogForm extends Component {
+type State = {
+  nameInput: string;
+  descriptionInput: string;
+  imageInput: string;
+};
+
+type Props = {
+  isLoading: boolean;
+  createDog: (dog: Omit<Dog, "id">) => void;
+};
+
+export class ClassCreateDogForm extends Component<Props, State> {
+  state: State = {
+    nameInput: "",
+    descriptionInput: "",
+    imageInput: `${dogPictures.BlueHeeler}`,
+  };
+
+  isValidName = () => (this.state.nameInput.length > 3); 
+  isValidDescrtiption = () => (this.state.descriptionInput.length > 3); 
+  isValidDog = () => (this.isValidName() && this.isValidDescrtiption());
+
   render() {
+    const { isLoading, createDog } = this.props;
+    const { nameInput, descriptionInput, imageInput } = this.state;
     return (
       <form
         action=""
         id="create-dog-form"
         onSubmit={(e) => {
           e.preventDefault();
+          createDog({
+            name: nameInput,
+            description: descriptionInput,
+            image: imageInput,
+            isFavorite: false,
+          });
+          this.setState({
+            nameInput: "",
+            descriptionInput: "",
+            imageInput: `${dogPictures.BlueHeeler}`,
+          });
         }}
       >
         <h4>Create a New Dog</h4>
         <label htmlFor="name">Dog Name</label>
-        <input type="text" onChange={() => {}} disabled={false} />
+        <input
+          type="text"
+          name="nameInput"
+          id="nameInput"
+          value={nameInput}
+          onChange={(e) => {
+            this.setState({ nameInput: e.target.value });
+          }}
+          disabled={isLoading}
+        />
         <label htmlFor="description">Dog Description</label>
         <textarea
-          name=""
-          id=""
+          name="description"
+          id="description"
+          value={descriptionInput}
           cols={80}
           rows={10}
-          onChange={(e) => {}}
-          disabled={false}
+          onChange={(e) => {
+            this.setState({ descriptionInput: e.target.value });
+          }}
+          disabled={isLoading}
         />
         <label htmlFor="picture">Select an Image</label>
-        <select onChange={(e) => {}} disabled={false}>
+        <select
+          name="picture"
+          id="picture"
+          value={imageInput}
+          onChange={(e) => {
+            this.setState({ imageInput: e.target.value });
+          }}
+          disabled={isLoading}
+        >
           {Object.entries(dogPictures).map(([label, pictureValue]) => {
             return (
               <option value={pictureValue} key={pictureValue}>
@@ -33,7 +88,7 @@ export class ClassCreateDogForm extends Component {
             );
           })}
         </select>
-        <input type="submit" value="submit" disabled={false} />
+        <input type="submit" value="submit" disabled={isLoading || !this.isValidDog()} />
       </form>
     );
   }
